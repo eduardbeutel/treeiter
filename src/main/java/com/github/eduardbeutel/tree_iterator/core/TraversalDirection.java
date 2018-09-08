9abb9b004c8a5bb1494c34f21fabe14868743089
Euard Beutel <eduard_beutel@yahoo.com>
2018-09-08 15:02:16 +0200

    Moving implementation to tree-iterator-core.

--- src/main/java/com/github/eduardbeutel/tree_iterator/core/TraversalDirection.java
+++
@@ -1,7 +0,0 @@
-package com.github.eduardbeutel.tree_iterator.core;
-
-public enum TraversalDirection
-{
-    TOP_DOWN,
-    BOTTOM_UP
-}
--- src/main/java/com/github/eduardbeutel/tree_iterator/document/OperationType.java
+++
@@ -1,6 +0,0 @@
-package com.github.eduardbeutel.tree_iterator.document;
-
-public enum OperationType
-{
-    NODE_CONSUMER // Consumer<Node>
-}
